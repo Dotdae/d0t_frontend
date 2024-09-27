aa15c2562cac5fa4d17a4353ab5f7790b383a3fb
Christopher Ortega <chriiswho@gmail.com>
2024-09-27 12:42:11 -0700

    Remove Employee component and added sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./shared/components/footer/footer.component";
-import { EmployeesComponent } from "./employees/employees.component";
+import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, EmployeesComponent],
+  imports: [RouterOutlet, FooterComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { EmployeesComponent } from './employees/employees.component';
 
 export const routes: Routes = [
 ];
