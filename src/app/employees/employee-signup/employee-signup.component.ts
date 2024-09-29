9f7389e0ba12755d8cd1250ab19b6ebddf04a9f4
Christopher Ortega <chriiswho@gmail.com>
2024-09-29 01:34:52 -0700

    Added rol option in employee sign up

--- src/app/employees/employee-signup/employee-signup.component.ts
+++ src/app/employees/employee-signup/employee-signup.component.ts
@@ -12,10 +12,23 @@ import { EmployeesService } from '../../core/services/employees.service';
 })
 export class EmployeeSignupComponent {
 
+
+
+  roles = [{
+    id: 1,
+    name: "Supervisor",
+  },{
+    id: 2,
+    name: "Empleado",
+  }];
+
+
   employeeName: string = '';
   employeeAge: number = 0;
   employeeAddress: string = '';
   employeeSalary: number = 0;
+  employeeRol: string = "";
+
 
   constructor(public api: EmployeesService){}
 
@@ -25,7 +38,8 @@ export class EmployeeSignupComponent {
       nombre: this.employeeName,
       edad: this.employeeAge,
       direccion: this.employeeAddress,
-      salario: this.employeeSalary
+      salario: this.employeeSalary,
+      rol: this.employeeRol
     }
 
     this.api.createEmployee(employee).subscribe((data) =>{
